b31683ea5aed42a1ae96369c4da0e60bda3a6e47
Judson Salinas <judz1105@gmail.com>
2021-09-16 21:40:46 -0500

    finished subCipherLogic

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Substitution.encrypt("flee at once. we are discovered!", "ZEBRASCDFGHIJKLMNOPQTUVWXY"));
+        System.out.println(Substitution.decrypt("SIAA ZQ LKBA. VA ZOA RFPBLUAOAR!", "ZEBRASCDFGHIJKLMNOPQTUVWXY"));
+    }
+
+}
--- src/Substitution.java
+++ src/Substitution.java
@@ -1,26 +1,52 @@
-
+import java.util.Locale;
 
 public class Substitution {
 
-
-    String[] alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-
-
-    public static String encrypt(String plainText, String key){
+    public static String encrypt(String pt, String k){
         String encrypted = "";
+        String plainText = pt.toUpperCase();
+        String key = k.toUpperCase();
 
         if(key.length() != 26){
             return "Alphabet Key must be 26 Characters!";
         }
 
         String[] keyAlphabet = new String[key.length()];
+
         for(int i = 0; i<key.length(); i++){
             keyAlphabet[i] = String.valueOf(key.charAt(i));
         }
 
-        for(int i = 0;)
+        for(int i = 0; i < plainText.length(); i++){
+            int index = plainText.charAt(i) - 65; // Subtract 65 cause ascii value offset
+
+            if(index > plainText.length() || index < 0){ // if its a special case then just leave it (were only changing alphabetic characters)
+                encrypted += String.valueOf((plainText.charAt(i)));
+            }else{
+                encrypted += keyAlphabet[index];
+            }
+        }
 
         return encrypted;
+    }
+
+    public static String decrypt(String ct, String k){
+        String decrypted = "";
+        String cipherText = ct.toUpperCase();
+        String key = k.toUpperCase();
+        for(int i = 0; i < cipherText.length(); i++) {
+
+            char character = cipherText.charAt(i);
+            int index = key.indexOf(character);
+            int val = index + 65; // + 65 to get ascii value (all upper case duh)
+
+            if (val < 65 || val > 90) { // boom special cases again
+                decrypted += String.valueOf(character); // just give the char cause its special case
+            } else {
+                decrypted += String.valueOf((char)val); // give the ascii value
+            }
+        }
+        return decrypted;
     }
 
 }
