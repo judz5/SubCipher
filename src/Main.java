821105270176ffa070fc3bb9ecc192b240600734
Judson Salinas <100000703@ccisd.net>
2021-10-01 10:33:34 -0500

    need to copy gui

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Runescape.encrypt("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 3));
+        System.out.println(Runescape.encrypt("THis is a test of this thing", 32));
+        System.out.println(Runescape.decrypt("uijt jt b uftu pg uijt uijoh", 32));
     }
 
 }
--- src/Runescape.java
+++ src/Runescape.java
@@ -8,6 +8,14 @@ public class Runescape {
 
     public static String encrypt(String pt, int tot){
         String plainText = pt.toLowerCase();
+
+        if(tot>2277){
+            return "2277 is max bud quit lying";
+        }
+        if(tot<32){
+            return "Lowest total level is 32";
+        }
+
         int key = tot/23;
 
         StringBuilder text = new StringBuilder();
@@ -15,6 +23,32 @@ public class Runescape {
             if(Character != ' '){
                 int ogPos = Character - 'a';
                 int newPos = (ogPos + key) % 26;
+                char newChar = (char)('a' + newPos);
+                text.append(newChar);
+            }else{
+                text.append(Character);
+            }
+        }
+        return text.toString();
+    }
+
+    public static String decrypt(String dt, int tot){
+        String plainText = dt.toLowerCase();
+
+        if(tot>2277){
+            return "2277 is max bud quit lying";
+        }
+        if(tot<32){
+            return "Lowest total level is 32";
+        }
+
+        int key = tot/23;
+
+        StringBuilder text = new StringBuilder();
+        for(char Character : plainText.toCharArray()){
+            if(Character != ' '){
+                int ogPos = Character - 'a';
+                int newPos = (ogPos - key) % 26;
                 char newChar = (char)('a' + newPos);
                 text.append(newChar);
             }else{
